37aa4e3ba57fb5c2ba2e5be7c12ab08b56b302eb
Cyrusssxx <cyrusssxx@gmail.com>
2026-08-22 15:10:02 +0800

    sw: PRECACHE 加入 selected.html/js/selected.json，CACHE_VER -> mathcards-22dc6bde00

--- pwa/sw.js
+++ pwa/sw.js
@@ -2,7 +2,7 @@
  * 预缓存全部页面/样式/脚本/笔记数据/KaTeX/图标，安装后完全离线可用。
  * 升级笔记数据或代码后：改 CACHE_VER 版本号即可让客户端自动换新缓存。
  */
-const CACHE_VER = 'mathcards-f8b6588feb';
+const CACHE_VER = 'mathcards-22dc6bde00';
 
 const PRECACHE = [
     'index.html',
@@ -17,6 +17,9 @@ const PRECACHE = [
     'js/annotate.js',
     'js/exam.js',
     'js/good.js',
+    'selected.html',
+    'js/selected.js',
+    'data/selected.json',
     'js/mdrender.js',
     'data/notes.json',
     'data/exam.json',
